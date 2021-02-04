a246a132fb1ac9dafba5075d695b0a0594369599
Carlos Rivero <carlosrivero96@gmail.com>
2021-02-03 22:07:20 -0400

    Added project-1

---
+++ project-1/src/software_renderer.cpp
@@ -0,0 +1,402 @@
+#include "software_renderer.h"
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+#include "triangulation.h"
+
+using namespace std;
+
+namespace CS248 {
+
+
+// Implements SoftwareRenderer //
+bool CompareFloat (float A, float B) {
+  float diff = A - B;
+  return (fabs(diff) < 0.00001);
+}
+// fill a sample location with color
+void SoftwareRendererImp::fill_sample(int sx, int sy, const Color &color) {
+  //return;
+
+  fill_pixel(sx, sy, color);
+  float inv255 = 1.0 / 255.0;
+
+  Color x1;
+  x1.r = render_target[4 * (sx +1+ sy * target_w)] * inv255;
+  x1.g = render_target[4 * (sx +1+ sy * target_w) + 1] * inv255;
+  x1.b = render_target[4 * (sx +1+ sy * target_w) + 2] * inv255;
+  x1.a = render_target[4 * (sx +1+ sy * target_w) + 3] * inv255;
+
+
+  Color x2;
+  x2.r = render_target[4 * (sx -1+ sy * target_w)] * inv255;
+  x2.g = render_target[4 * (sx -1+ sy * target_w) + 1] * inv255;
+  x2.b = render_target[4 * (sx -1+ sy * target_w) + 2] * inv255;
+  x2.a = render_target[4 * (sx -1+ sy * target_w) + 3] * inv255;
+
+
+  Color x3;
+  x3.r = render_target[4 * (sx + (sy+1) * target_w)] * inv255;
+  x3.g = render_target[4 * (sx + (sy+1) * target_w) + 1] * inv255;
+  x3.b = render_target[4 * (sx + (sy+1) * target_w) + 2] * inv255;
+  x3.a = render_target[4 * (sx + (sy+1) * target_w) + 3] * inv255;
+
+
+  Color x4;
+  x4.r = render_target[4 * (sx + (sy-1) * target_w)] * inv255;
+  x4.g = render_target[4 * (sx + (sy-1) * target_w) + 1] * inv255;
+  x4.b = render_target[4 * (sx + (sy-1) * target_w) + 2] * inv255;
+  x4.a = render_target[4 * (sx + (sy-1) * target_w) + 3] * inv255;
+/*
+  printf("%f %f %f %f\n", x1.r, x1.g, x1.b, x1.a);
+  printf("%f %f %f %f\n", color.r, color.g, color.b, color.a);
+  printf("pupu\n");
+*/
+  
+  if (!((CompareFloat(x1.r, color.r)) && (CompareFloat(x1.g, color.g)) 
+  && (CompareFloat(x1.b, color.b)) && (CompareFloat(x1.a, color.a)))) {
+    fill_sample(sx+1, sy, color);
+  }
+  if (!((CompareFloat(x2.r, color.r)) && (CompareFloat(x2.g, color.g)) 
+  && (CompareFloat(x2.b, color.b)) && (CompareFloat(x2.a, color.a)))) {
+    fill_sample(sx-1, sy, color);
+  }
+  if (!((CompareFloat(x3.r, color.r)) && (CompareFloat(x3.g, color.g)) 
+  && (CompareFloat(x3.b, color.b)) && (CompareFloat(x3.a, color.a)))) {
+    fill_sample(sx, sy+1, color);
+  }
+  if (!((CompareFloat(x4.r, color.r)) && (CompareFloat(x4.g, color.g)) 
+  && (CompareFloat(x4.b, color.b)) && (CompareFloat(x4.a, color.a)))) {
+    fill_sample(sx, sy-1, color);
+  }
+  
+}
+
+// fill samples in the entire pixel specified by pixel coordinates
+void SoftwareRendererImp::fill_pixel(int x, int y, const Color &color) {
+
+	// Task 2: Re-implement this function
+
+	// check bounds
+	if (x < 0 || x >= target_w) return;
+	if (y < 0 || y >= target_h) return;
+
+	Color pixel_color;
+	float inv255 = 1.0 / 255.0;
+	pixel_color.r = render_target[4 * (x + y * target_w)] * inv255;
+	pixel_color.g = render_target[4 * (x + y * target_w) + 1] * inv255;
+	pixel_color.b = render_target[4 * (x + y * target_w) + 2] * inv255;
+	pixel_color.a = render_target[4 * (x + y * target_w) + 3] * inv255;
+
+	pixel_color = ref->alpha_blending_helper(pixel_color, color);
+
+	render_target[4 * (x + y * target_w)] = (uint8_t)(pixel_color.r * 255);
+	render_target[4 * (x + y * target_w) + 1] = (uint8_t)(pixel_color.g * 255);
+	render_target[4 * (x + y * target_w) + 2] = (uint8_t)(pixel_color.b * 255);
+	render_target[4 * (x + y * target_w) + 3] = (uint8_t)(pixel_color.a * 255);
+
+}
+
+void SoftwareRendererImp::draw_svg( SVG& svg ) {
+
+  // set top level transformation
+  transformation = canvas_to_screen;
+
+  // draw all elements
+  for ( size_t i = 0; i < svg.elements.size(); ++i ) {
+    draw_element(svg.elements[i]);
+  }
+
+  // draw canvas outline
+  Vector2D a = transform(Vector2D(    0    ,     0    )); a.x--; a.y--;
+  Vector2D b = transform(Vector2D(svg.width,     0    )); b.x++; b.y--;
+  Vector2D c = transform(Vector2D(    0    ,svg.height)); c.x--; c.y++;
+  Vector2D d = transform(Vector2D(svg.width,svg.height)); d.x++; d.y++;
+
+  rasterize_line(a.x, a.y, b.x, b.y, Color::Black);
+  rasterize_line(a.x, a.y, c.x, c.y, Color::Black);
+  rasterize_line(d.x, d.y, b.x, b.y, Color::Black);
+  rasterize_line(d.x, d.y, c.x, c.y, Color::Black);
+
+  // resolve and send to render target
+  resolve();
+
+}
+
+void SoftwareRendererImp::set_sample_rate( size_t sample_rate ) {
+
+  // Task 2: 
+  // You may want to modify this for supersampling support
+  this->sample_rate = sample_rate;
+
+}
+
+void SoftwareRendererImp::set_render_target( unsigned char* render_target,
+                                             size_t width, size_t height ) {
+
+  // Task 2: 
+  // You may want to modify this for supersampling support
+  this->render_target = render_target;
+  this->target_w = width;
+  this->target_h = height;
+
+}
+
+void SoftwareRendererImp::draw_element( SVGElement* element ) {
+
+	// Task 3 (part 1):
+	// Modify this to implement the transformation stack
+
+	switch (element->type) {
+	case POINT:
+		draw_point(static_cast<Point&>(*element));
+		break;
+	case LINE:
+		draw_line(static_cast<Line&>(*element));
+		break;
+	case POLYLINE:
+		draw_polyline(static_cast<Polyline&>(*element));
+		break;
+	case RECT:
+		draw_rect(static_cast<Rect&>(*element));
+		break;
+	case POLYGON:
+		draw_polygon(static_cast<Polygon&>(*element));
+		break;
+	case ELLIPSE:
+		draw_ellipse(static_cast<Ellipse&>(*element));
+		break;
+	case IMAGE:
+		draw_image(static_cast<Image&>(*element));
+		break;
+	case GROUP:
+		draw_group(static_cast<Group&>(*element));
+		break;
+	default:
+		break;
+	}
+
+}
+
+
+// Primitive Drawing //
+
+void SoftwareRendererImp::draw_point( Point& point ) {
+
+  Vector2D p = transform(point.position);
+  rasterize_point( p.x, p.y, point.style.fillColor );
+
+}
+
+void SoftwareRendererImp::draw_line( Line& line ) { 
+
+  Vector2D p0 = transform(line.from);
+  Vector2D p1 = transform(line.to);
+  rasterize_line( p0.x, p0.y, p1.x, p1.y, line.style.strokeColor );
+
+}
+
+void SoftwareRendererImp::draw_polyline( Polyline& polyline ) {
+
+  Color c = polyline.style.strokeColor;
+
+  if( c.a != 0 ) {
+    int nPoints = polyline.points.size();
+    for( int i = 0; i < nPoints - 1; i++ ) {
+      Vector2D p0 = transform(polyline.points[(i+0) % nPoints]);
+      Vector2D p1 = transform(polyline.points[(i+1) % nPoints]);
+      rasterize_line( p0.x, p0.y, p1.x, p1.y, c );
+    }
+  }
+}
+
+void SoftwareRendererImp::draw_rect( Rect& rect ) {
+
+  Color c;
+  
+  // draw as two triangles
+  float x = rect.position.x;
+  float y = rect.position.y;
+  float w = rect.dimension.x;
+  float h = rect.dimension.y;
+
+  Vector2D p0 = transform(Vector2D(   x   ,   y   ));
+  Vector2D p1 = transform(Vector2D( x + w ,   y   ));
+  Vector2D p2 = transform(Vector2D(   x   , y + h ));
+  Vector2D p3 = transform(Vector2D( x + w , y + h ));
+  
+  // draw fill
+  c = rect.style.fillColor;
+  if (c.a != 0 ) {
+    rasterize_triangle( p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, c );
+    rasterize_triangle( p2.x, p2.y, p1.x, p1.y, p3.x, p3.y, c );
+  }
+
+  // draw outline
+  c = rect.style.strokeColor;
+  if( c.a != 0 ) {
+    rasterize_line( p0.x, p0.y, p1.x, p1.y, c );
+    rasterize_line( p1.x, p1.y, p3.x, p3.y, c );
+    rasterize_line( p3.x, p3.y, p2.x, p2.y, c );
+    rasterize_line( p2.x, p2.y, p0.x, p0.y, c );
+  }
+
+}
+
+void SoftwareRendererImp::draw_polygon( Polygon& polygon ) {
+
+  Color c;
+
+  // draw fill
+  c = polygon.style.fillColor;
+  if( c.a != 0 ) {
+
+    // triangulate
+    vector<Vector2D> triangles;
+    triangulate( polygon, triangles );
+
+    // draw as triangles
+    for (size_t i = 0; i < triangles.size(); i += 3) {
+      Vector2D p0 = transform(triangles[i + 0]);
+      Vector2D p1 = transform(triangles[i + 1]);
+      Vector2D p2 = transform(triangles[i + 2]);
+      rasterize_triangle( p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, c );
+    }
+  }
+
+  // draw outline
+  c = polygon.style.strokeColor;
+  if( c.a != 0 ) {
+    int nPoints = polygon.points.size();
+    for( int i = 0; i < nPoints; i++ ) {
+      Vector2D p0 = transform(polygon.points[(i+0) % nPoints]);
+      Vector2D p1 = transform(polygon.points[(i+1) % nPoints]);
+      rasterize_line( p0.x, p0.y, p1.x, p1.y, c );
+    }
+  }
+}
+
+void SoftwareRendererImp::draw_ellipse( Ellipse& ellipse ) {
+
+  // Extra credit 
+
+}
+
+void SoftwareRendererImp::draw_image( Image& image ) {
+
+  Vector2D p0 = transform(image.position);
+  Vector2D p1 = transform(image.position + image.dimension);
+
+  rasterize_image( p0.x, p0.y, p1.x, p1.y, image.tex );
+}
+
+void SoftwareRendererImp::draw_group( Group& group ) {
+
+  for ( size_t i = 0; i < group.elements.size(); ++i ) {
+    draw_element(group.elements[i]);
+  }
+
+}
+
+// Rasterization //
+
+// The input arguments in the rasterization functions 
+// below are all defined in screen space coordinates
+
+void SoftwareRendererImp::rasterize_point( float x, float y, Color color ) {
+
+  // fill in the nearest pixel
+  int sx = (int)floor(x);
+  int sy = (int)floor(y);
+
+  // check bounds
+  if (sx < 0 || sx >= target_w) return;
+  if (sy < 0 || sy >= target_h) return;
+
+  // fill sample - NOT doing alpha blending!
+  // TODO: Call fill_pixel here to run alpha blending
+  render_target[4 * (sx + sy * target_w)] = (uint8_t)(color.r * 255);
+  render_target[4 * (sx + sy * target_w) + 1] = (uint8_t)(color.g * 255);
+  render_target[4 * (sx + sy * target_w) + 2] = (uint8_t)(color.b * 255);
+  render_target[4 * (sx + sy * target_w) + 3] = (uint8_t)(color.a * 255);
+
+}
+
+void SoftwareRendererImp::rasterize_line( float x0, float y0,
+                                          float x1, float y1,
+                                          Color color) {
+
+  // Extra credit (delete the line below and implement your own)
+  //ref->rasterize_line_helper(x0, y0, x1, y1, target_w, target_h, color, this);
+
+  int xx0 = round(x0);
+  int xx1 = round(x1);
+  int yy0 = round(y0);
+  int yy1 = round(y1);
+
+  int dx = abs(xx1 - xx0);
+  int sx = xx0<xx1 ? 1 : -1;
+  int dy = -abs(yy1 - yy0);
+  int sy = yy0<yy1 ? 1 : -1;
+  int err = dx+dy;
+  while(true) {
+    rasterize_point(xx0, yy0, color);
+    if (xx0 == xx1 && yy0 == yy1) break;
+    int e2 = 2*err;
+    if (e2 >= dy){
+      err += dy;
+      xx0 += sx;
+    }
+    
+    if (e2 <= dx) {
+      err += dx;
+      yy0 += sy;
+    }
+  }
+  
+}
+
+void SoftwareRendererImp::rasterize_triangle( float x0, float y0,
+                                              float x1, float y1,
+                                              float x2, float y2,
+                                              Color color ) {
+  // Task 1: 
+  // Implement triangle rasterization (you may want to call fill_sample here)
+  rasterize_line(x0, y0, x1, y1, color);
+  rasterize_line(x1, y1, x2, y2, color);
+  rasterize_line(x2, y2, x0, y0, color);
+  float xc = (x0+x1+x2)/3;
+  float yc = (y0+y1+y2)/3;
+  fill_sample(int(xc), int(yc), color);
+}
+
+void SoftwareRendererImp::rasterize_image( float x0, float y0,
+                                           float x1, float y1,
+                                           Texture& tex ) {
+  // Task 4: 
+  // Implement image rasterization (you may want to call fill_sample here)
+
+}
+
+// resolve samples to render target
+void SoftwareRendererImp::resolve( void ) {
+
+  // Task 2: 
+  // Implement supersampling
+  // You may also need to modify other functions marked with "Task 2".
+  return;
+
+}
+
+Color SoftwareRendererImp::alpha_blending(Color pixel_color, Color color)
+{
+  // Task 5
+  // Implement alpha compositing
+  return pixel_color;
+}
+
+
+} // namespace CS248
