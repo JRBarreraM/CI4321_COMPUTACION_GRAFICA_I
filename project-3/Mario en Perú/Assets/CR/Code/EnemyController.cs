2cf07736421926a7bc6c798c774ce193e74c1d1a
carlosrivero96 <carlosrivero96@gmail.com>
2021-04-17 22:27:06 -0400

    Added enemyMovement

---
+++ project-3/Mario en Perú/Assets/CR/Code/EnemyController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float moveSpeed = 50f;
+    private float direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction * moveSpeed * Time.deltaTime,GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Wall"))
+        {
+            direction *= -1;
+        }
+    }
+}
